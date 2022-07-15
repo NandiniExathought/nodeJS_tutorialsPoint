bee99235ccdbf6abb2d8262670181140f07fa951
Nandini Gnanesh <nandini.gnanesh@exathought.com>
2022-07-15 12:57:51 +0530

    modified commends.log

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-   res.send('Sita');
+   res.send('Hello World!');
 })
 
 var server = app.listen(8081, function () {
