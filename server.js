86f8a683aa5977aeac29f25b3cfa4b5767275288
Nandini Gnanesh <nandini.gnanesh@exathought.com>
2022-07-15 16:56:10 +0530

    simple example which passes two values using HTML FORM POST method

--- server.js
+++ server.js
@@ -82,6 +82,7 @@ var server = app.listen(8081, function () {
 
 */
 
+/*
 var express = require('express');
 var app = express();
 
@@ -101,6 +102,38 @@ app.get('/process_get', function (req, res) {
  })
 
  var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+http://127.0.0.1:8081/index.html
+*/
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
