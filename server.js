138d3d339642c1c2273bb5e820d03d48f999747c
Nandini Gnanesh <nandini.gnanesh@exathought.com>
2022-07-15 16:36:59 +0530

    add example which passes two values using HTML FORM GET method

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ var server = app.listen(8081, function () {
  })
 
  */
-
+/*
 var express = require('express');
 var app = express();
 
@@ -75,5 +75,34 @@ var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
 
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+//http://127.0.0.1:8081/images/Lays-Logo.png
+
+*/
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+       first_name:req.query.first_name,
+       last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+ })
+
+ var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
    console.log("Example app listening at http://%s:%s", host, port)
 })
